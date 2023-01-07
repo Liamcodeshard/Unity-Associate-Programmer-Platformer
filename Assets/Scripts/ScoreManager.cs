53d64d395f22ff174a282d249039d844fa61980a
Liamcodeshard <liam.ashton@hotmail.co.uk>
2023-01-07 17:03:14 +0000

    Initial commit

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int score;
+
+
+    void Start()
+    {
+        // resets the score to 0 at the start of the game
+        score = 0;
+
+    }
+}
